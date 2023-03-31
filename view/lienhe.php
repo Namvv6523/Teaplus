19823463a283849b91283eac4a208005d7b97db2
namvv <namvv@gmail.com>
2023-03-31 19:01:54 +0700

    add lienhe.php

---
+++ view/lienhe.php
@@ -0,0 +1,19 @@
+
+    <div class="container-lienhe">
+        <div class="container-lienhe_left"></div>
+        <div class="container-lienhe_right">
+            <div class="container-lienhe_right_form">
+            <h1 class="container-lienhe_right_form_title">Liên hệ</h1>
+            <ul class="container-lienhe_right_form_data">
+            <li><p>Họ và Tên</li></p> <input placeholder="Họ và Tên" class="container-lienhe_right_form_text" type="text"></li>
+            <li><p>Địa chỉ</p> <input placeholder="Địa chỉ..."class="container-lienhe_right__form_text" type="text"></li>
+            <li><p>Điện thoại<p> <input placeholder="Điện thoại" class="container-lienhe_right_form_text" type="text"></li>
+            <li><p>Email</p> <input placeholder="Email..." class="container-lienhe_right_form_text" type="text"></li>
+            <li><p>Lời nhắn</p> <textarea name="" id="" cols="30" rows="2"></textarea></li>
+            </ul>
+            <button class="container-lienhe_right_form_bt">Gửi liên hệ</button>
+            <button class="container-lienhe_right_form_bt">Soạn lại</button>
+            </div>
+        </div>
+    </div>
+    
